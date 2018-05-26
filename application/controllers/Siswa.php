967b0bef4388b1b87641036fe7bd432f4f91b40d
AnisaDyah <38083115+AnisaDyah@users.noreply.github.com>
2018-05-25 21:30:30 -0700

    add pagination

--- application/controllers/Siswa.php
+++ application/controllers/Siswa.php
@@ -10,21 +10,54 @@ class Siswa extends CI_Controller {
 {
   parent::__construct();
   $this->load->helper('url', 'form');
-  $this->load->library('form_validation');
+  $this->load->library('form_validation','pagination');
   $this->load->model('siswa_model');
 }
     public function index()
     {
+      $total = $this->siswa_model->getTotal();
+      if ($total > 0) {
+                    $limit = 2;
+                    $start = $this->uri->segment(3, 0);
         
+                    $config = [
+                      'base_url' => base_url() . 'siswa/index',
+                      'total_rows' => $total,
+                      'per_page' => $limit,
+                     'uri_segment' => 3,
+      
+                      // Bootstrap 3 Pagination
+                      'first_link' => '&laquo;',
+                      'last_link' => '&raquo;',
+                      'next_link' => 'Next',
+                      'prev_link' => 'Prev',
+                      'full_tag_open' => '<ul class="pagination">',
+                      'full_tag_close' => '</ul>',
+                      'num_tag_open' => '<li>',
+                      'num_tag_close' => '</li>',
+                      'cur_tag_open' => '<li class="active"><span>',
+                      'cur_tag_close' => '<span class="sr-only">(current)</span></span></li>',
+                      'next_tag_open' => '<li>',
+                      'next_tag_close' => '</li>',
+                      'prev_tag_open' => '<li>',
+                      'prev_tag_close' => '</li>',
+                      'first_tag_open' => '<li>',
+                      'first_tag_close' => '</li>',
+                      'last_tag_open' => '<li>',
+                      'last_tag_close' => '</li>',
+                   ];
+                    $this->pagination->initialize($config);
+
         $this->load->model('siswa_model');
-        
-        $siswa= $this->siswa_model->list();
+        $siswa= $this->siswa_model->list($limit,$start);
         $jenis_siswa = $this->siswa_model->jenis_siswa();
   $data = [
     'title' => 'Pemrograman Web Framework :: Data siswa',
     'siswa' => $siswa,
-    'jenis_siswa' => $jenis_siswa
+    'jenis_siswa' => $jenis_siswa,
+    'links' => $this->pagination->create_links()
   ];
+}
   $this->load->view('siswa/index', $data);
     }
 
